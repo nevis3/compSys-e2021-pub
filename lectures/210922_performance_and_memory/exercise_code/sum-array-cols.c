4fe15d629dc0ebc34da127286b176cf87a72bd11
svenh <svenhelbig3@gmail.com>
2021-10-11 20:48:10 +0200

    Safety commit

--- lectures/210922_performance_and_memory/exercise_code/sum-array-cols.c
+++ lectures/210922_performance_and_memory/exercise_code/sum-array-cols.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//definerer at BAD_INPUT ikke må være et 0 dimensionel array
 #define BAD_INPUT 0
 
 long sum_array_cols(long *a, int M, int N)
@@ -11,23 +12,29 @@ long sum_array_cols(long *a, int M, int N)
   for (j = 0; j < N; j++) {
     for (i = 0; i < M; i++) {
       sum += a[i * M + j];
+      printf("%lu\n", sum);
     }
   }
   return sum;
 }
 
+
+//the input defines the size of the matrix not actual values to be stored
+//therefore ./cols 2 2 allocates memory for a 2x2 matrix with 0's in each place
 int main (int argc, char* argv[]) {
   unsigned long n,m; // Array size
-  if (argc != 3) { // 2 argument
+  if (argc != 3) { // 2 arguments needed
     printf("I want two ints as arguments!\n");
     return EXIT_SUCCESS;
   }
+
   int res;
   res = sscanf(argv[1],"%lu", &m);
   if (res == BAD_INPUT) {
     printf("I want two ints as arguments!\n");
     return EXIT_SUCCESS;
   }
+
   res = sscanf(argv[1],"%lu", &n);
   if (res == BAD_INPUT) {
     printf("I want two ints as arguments!\n");
@@ -37,6 +44,13 @@ int main (int argc, char* argv[]) {
   // Give me some memory with the size of the array
   long* input = (long*) malloc(m * n * sizeof(long));
   // Note, I do not care about the input
+
+  //adding values to the matrix, can be removed, resulting in 0's
+  int i;
+  for (i = 0; i < m*n; i++)
+  {
+    input[i] = i;
+  }
 
   // use volatile to prevent compiler from optimizing this away!
   volatile long l = sum_array_cols(input, m, n);
